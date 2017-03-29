7f269898853cdccff4177a11389d2b8b8866d76b
Hafiz Wahid <hafizwahid@winapp.my>
2017-03-29 12:25:06 +0800

    Initial Commit

--- scrollstackview/ViewController.swift
+++ scrollstackview/ViewController.swift
@@ -1,6 +1,6 @@
 //
 //  ViewController.swift
-//  scrollstackview
+//  
 //
 //  Created by Hafiz Wahid on 29/03/2017.
 //  Copyright © 2017 hw. All rights reserved.
@@ -9,17 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var scrollView: UIScrollView!
+    var stackView: UIStackView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(scrollView)
+        
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[scrollView]|", options: .alignAllCenterX, metrics: nil, views: ["scrollView": scrollView]))
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[scrollView]|", options: .alignAllCenterX, metrics: nil, views: ["scrollView": scrollView]))
+        
+        
+        stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        scrollView.addSubview(stackView)
+        
+        scrollView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[stackView]|", options: NSLayoutFormatOptions.alignAllCenterX, metrics: nil, views: ["stackView": stackView]))
+        scrollView.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[stackView]", options: NSLayoutFormatOptions.alignAllCenterX, metrics: nil, views: ["stackView": stackView]))
+        
+        for _ in 1 ..< 100 {
+            let vw = UIButton(type: UIButtonType.system)
+            vw.setTitle("Button", for: .normal)
+            stackView.addArrangedSubview(vw)
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        scrollView.contentSize = CGSize(width: stackView.frame.width, height: stackView.frame.height)		
     }
-
-
 }
 
